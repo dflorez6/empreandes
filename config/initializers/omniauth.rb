3fef80f449be3ee12fd5f442bbaf23e023f2d948
Jose Striedinger <jstriedinger2090@gmail.com>
2013-05-06 03:07:07 -0500

    assets precompiled

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '558350127520029', '04e8b4a6bff19a3d479dc47ffdfa31a8',{:client_options => {:ssl => {:verify => false}}}
 end
+
+require 'event_calendar'
+ActiveRecord::Base.send :include, EventCalendar
+ActionView::Base.send :include, EventCalendar::CalendarHelper
